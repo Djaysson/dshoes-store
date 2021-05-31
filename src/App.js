8111c152fa56d3a0e17e8324388055506e7a1933
Djaysson <djayson.dev@gmail.com>
2021-05-31 18:00:56 -0300

    Creating the project header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
-
+import Header from './components/Header/index';
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
       <GlobalStyle />
     </BrowserRouter>
