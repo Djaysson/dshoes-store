055dfb144c38e63bb1ddd58448b2f65bb3c1cda1
Djaysson <djayson.dev@gmail.com>
2021-06-09 19:53:01 -0300

    Listing products in cart using Redux

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import img from '../../assets/images/shoes1.jpg';
-import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md'
+import { useSelector } from 'react-redux';
 
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md'
 
 import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
+    const cart = useSelector((state) => state.cart)
     return (
         <Container>
             <ProductTable>
@@ -19,34 +20,36 @@ export default function Cart() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>
-                            <img src={img} alt="" />
-                        </td>
-                        <td>
-                            <strong>Tênis muito massa</strong>
-                            <span>R$129,90</span>
-                        </td>
-                        <td>
-                            <div>
+                    {cart.map(product => (
+                        <tr>
+                            <td>
+                                <img src={product.image} alt={product.title} />
+                            </td>
+                            <td>
+                                <strong>{product.title}</strong>
+                                <span>{product.priceFormatted}</span>
+                            </td>
+                            <td>
+                                <div>
+                                    <button type="buttton">
+                                        <MdRemoveCircleOutline size={20} color="#1521b3" />
+                                    </button>
+                                    <input type="number" readOnly value={product.amount} />
+                                    <button type="buttton">
+                                        <MdAddCircleOutline size={20} color="#1521b3" />
+                                    </button>
+                                </div>
+                            </td>
+                            <td>
+                                <strong>R$259,80</strong>
+                            </td>
+                            <td>
                                 <button type="buttton">
-                                    <MdRemoveCircleOutline size={20} color="#1521b3" />
+                                    <MdDelete size={20} color="#1521b3" />
                                 </button>
-                                <input type="number" readOnly value={2} />
-                                <button type="buttton">
-                                    <MdAddCircleOutline size={20} color="#1521b3" />
-                                </button>
-                            </div>
-                        </td>
-                        <td>
-                            <strong>R$259,80</strong>
-                        </td>
-                        <td>
-                            <button type="buttton">
-                                <MdDelete size={20} color="#1521b3" />
-                            </button>
-                        </td>
-                    </tr>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </ProductTable>
             <footer>
